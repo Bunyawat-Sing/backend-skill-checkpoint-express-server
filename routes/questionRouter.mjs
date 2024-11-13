1099dd924773b6fbbe8121461cbfc2d683105a65
Bunyawat Singkiporn <pupahero@gmail.com>
2024-11-13 14:39:18 +0700

    feat: complete create read update delete requirement

--- routes/questionRouter.mjs
+++ routes/questionRouter.mjs
@@ -3,29 +3,134 @@ import { Router } from "express";
 
 const questionRouter = Router();
 
+//create question
 questionRouter.post("/", async (req, res) => {
   try {
-    const newQuestion = { ...req.body};
-    const result = await connectionPool.query(
-      `INSERT INTO questions (title, description, category) VALUES ($1,$2,$3) RETURNING id`,
-      [
-        newQuestion.title,
-        newQuestion.description,
-        newQuestion.category,
-      ]
+    const { title, description, category } = req.body;
+    const newQuestion = { ...req.body };
+
+    await connectionPool.query(
+      `INSERT INTO questions (title, description, category) VALUES ($1,$2,$3)`,
+      [newQuestion.title, newQuestion.description, newQuestion.category]
     );
 
-    const newQuestionId = result.rows[0].id;
+    if (!title || !description || !category) {
+      return res.status(400).json({
+        message: "Invalid request data.",
+      });
+    }
+
     return res.status(201).json({
-      message: `Question id: ${newQuestionId} has been created successfully`,
+      message: `Question created successfully.`,
     });
-  } catch (error) {
-    console.error("Database error:", error); 
+  } catch {
     return res.status(500).json({
-      message: `Server could not create post due to a database error`,
-      error: error.message, 
+      message: `Unable to create question.`,
     });
   }
 });
+
+//read all question
+questionRouter.get("/", async (req, res) => {
+  try {
+    const result = await connectionPool.query(
+      `SELECT id, title, description, category FROM questions`
+    );
+
+    return res.status(200).json({ data: result.rows });
+  } catch {
+    return res.status(500).json({
+      message: `Unable to fetch questions.`,
+    });
+  }
+});
+
+//read question by id
+questionRouter.get("/:questionId", async (req, res) => {
+  try {
+    const questionIdFromClient = req.params.questionId;
+    const result = await connectionPool.query(
+      `SELECT id, title, description, category FROM questions WHERE id = $1`,
+      [questionIdFromClient]
+    );
+
+    if (!result.rows[0]) {
+      return res.status(404).json({
+        message: `Question not found.`,
+      });
+    }
+
+    return res.status(200).json({ data: result.rows });
+  } catch {
+    return res.status(500).json({
+      message: `Unable to fetch questions.`,
+    });
+  }
+});
+
+//update question by id
+questionRouter.put("/:questionId", async (req, res) => {
+  try {
+    const questionIdFromClient = req.params.questionId;
+    const { title, description, category } = req.body;
+    const updatedQuestion = { ...req.body };
+    const result = await connectionPool.query(
+      `UPDATE questions SET title = $2, description = $3, category = $4 WHERE id = $1`,
+      [
+        questionIdFromClient,
+        updatedQuestion.title,
+        updatedQuestion.description,
+        updatedQuestion.category,
+      ]
+    );
+
+    if (!title || !description || !category) {
+      return res.status(400).json({
+        message: "Invalid request data.",
+      });
+    }
+
+    if (!result.rowCount) {
+      return res.status(404).json({
+        message: `Question not found.`,
+      });
+    }
+
+    return res.status(200).json({ message: "Question updated successfully." });
+  } catch {
+    return res.status(500).json({
+      message: `Unable to fetch questions.`,
+    });
+  }
+});
+
+//delete question
+questionRouter.delete("/:questionId", async (req, res) => {
+  try {
+    const questionIdFromClient = req.params.questionId;
+
+    const result = await connectionPool.query(
+      `DELETE FROM questions WHERE id = $1`,
+      [questionIdFromClient]
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(404).json({
+        message: `Question not found.`,
+      });
+    }
+
+    return res.status(200).json({
+      message: "Question post has been deleted successfully.",
+    });
+  } catch {
+    return res.status(500).json({
+      message: "Unable to delete question.",
+    });
+  }
+});
+
+//search question by title or category
+
 
 export default questionRouter;
