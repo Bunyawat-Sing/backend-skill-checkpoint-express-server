66095e7be4ed038e189914f3bf0396edb5975cb4
Bunyawat Singkiporn <pupahero@gmail.com>
2024-11-13 15:19:22 +0700

    feat: add api search

--- routes/questionRouter.mjs
+++ routes/questionRouter.mjs
@@ -3,6 +3,43 @@ import { Router } from "express";
 
 const questionRouter = Router();
 
+//search question by title or category
+questionRouter.get("/search", async (req, res) => {
+  try {
+    let query = `SELECT id, title, description, category FROM questions `;
+    let values = [];
+    const category = req.query.category || "";
+    const title = req.query.title || "";
+
+    if (title && category) {
+      query += ` WHERE category ILIKE $1 AND title ILIKE $2`;
+      values = [`%${category}%`, `%${title}%`];
+    } else if (category) {
+      query += ` WHERE category ILIKE $1`;
+      values = [`%${category}%`];
+    } else if (title) {
+      query += ` WHERE title ILIKE $1`;
+      values = [`%${title}%`];
+    }
+
+    const result = await connectionPool.query(query, values);
+
+    if (!category && !title) {
+      return res.status(400).json({
+        message: "Invalid search parameters.",
+      });
+    }
+
+    return res.status(200).json({
+      data: result.rows,
+    });
+  } catch {
+    return res.status(500).json({
+      message: "Unable to fetch questions.",
+    });
+  }
+});
+
 //create question
 questionRouter.post("/", async (req, res) => {
   try {
@@ -129,8 +166,5 @@ questionRouter.delete("/:questionId", async (req, res) => {
     });
   }
 });
-
-//search question by title or category
-
 
 export default questionRouter;
